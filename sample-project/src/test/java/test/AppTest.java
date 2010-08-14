a49208f77060ef3bdd40118c09d856389cb842f5
kohsuke <kohsuke@dev.java.net>
2010-08-14 22:23:57 +0000

    test project that uses SeleniumFactory

---
+++ sample-project/src/test/java/test/AppTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import com.saucelabs.selenium.client.client.factory.SeleniumFactory;
+import com.thoughtworks.selenium.Selenium;
+import junit.framework.TestCase;
+
+public class AppTest extends TestCase
+{
+    public void test1() throws Exception {
+        Selenium s = SeleniumFactory.create("http://www.google.com/");
+        s.start();
+        s.open("/");
+        assertEquals("Google",s.getTitle());
+        s.stop();
+    }
+
+    public void test2() throws Exception {
+        Selenium s = SeleniumFactory.create("http://www.yahoo.com/");
+        s.start();
+        s.open("/");
+        assertEquals("Yahoo!",s.getTitle());
+        s.stop();
+    }
+}
