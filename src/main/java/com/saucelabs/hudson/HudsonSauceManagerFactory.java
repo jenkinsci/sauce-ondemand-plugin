a496e5fe367728d8c6063f869bd43dff91650e92
Ross Rowe <piaross@gmail.com>
2012-02-23 21:59:58 +1100

    Added lock to creation of Sauce Manager

--- src/main/java/com/saucelabs/hudson/HudsonSauceManagerFactory.java
+++ src/main/java/com/saucelabs/hudson/HudsonSauceManagerFactory.java
@@ -7,6 +7,9 @@ import org.codehaus.plexus.PlexusContainer;
 import org.codehaus.plexus.PlexusContainerException;
 import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author Ross Rowe
  */
@@ -16,20 +19,23 @@ public class HudsonSauceManagerFactory {
 
     private PlexusContainer plexus = null;
 
+    private Lock accessLock = new ReentrantLock();
+
     public static HudsonSauceManagerFactory getInstance() {
         return INSTANCE;
     }
 
-
     private HudsonSauceManagerFactory() {
-
     }
 
     public SauceConnectTwoManager createSauceConnectManager() throws ComponentLookupException {
-        if (plexus == null) {
+        accessLock.lock();
+        try {
             start();
+            return (SauceConnectTwoManager) this.plexus.lookup(SauceTunnelManager.class.getName());
+        } finally {
+            accessLock.unlock();
         }
-        return (SauceConnectTwoManager) this.plexus.lookup(SauceTunnelManager.class.getName());
     }
 
     public void start() {
@@ -40,9 +46,9 @@ public class HudsonSauceManagerFactory {
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
             try {
-// These will only be useful for Hudson v1.395 and under
-// ... Since the use of sisu-plexus-inject will initialize
-// everything in the constructor
+                // These will only be useful for Hudson v1.395 and under
+                // ... Since the use of sisu-plexus-inject will initialize
+                // everything in the constructor
                 PlexusContainer.class.getDeclaredMethod("initialize").invoke(this.plexus);
                 PlexusContainer.class.getDeclaredMethod("start").invoke(this.plexus);
             } catch (Throwable e) { /* Don't do anything here ... initialize/start methods should be called prior to v1.395 ! */ }
