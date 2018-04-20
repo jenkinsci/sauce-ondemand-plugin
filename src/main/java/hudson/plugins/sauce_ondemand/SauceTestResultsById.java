1a81dadc93f4d231b753db2fdff914c4f849549c
Yeh Fang <yeh.fang@saucelabs.com>
2018-04-20 15:21:14 -0700

    add junit stacktrace in more areas of the results UI

--- src/main/java/hudson/plugins/sauce_ondemand/SauceTestResultsById.java
+++ src/main/java/hudson/plugins/sauce_ondemand/SauceTestResultsById.java
@@ -3,6 +3,10 @@ package hudson.plugins.sauce_ondemand;
 import hudson.plugins.sauce_ondemand.credentials.SauceCredentials;
 
 import java.io.IOException;
+import org.json.JSONObject;
+import org.json.JSONException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -10,10 +14,27 @@ import java.io.IOException;
 public class SauceTestResultsById {
     private final String id;
     private final SauceCredentials credentials;
+    private final JenkinsJobInformation job;
+
+    /**
+     * Logger instance.
+     */
+    private static final Logger logger = Logger.getLogger(SauceTestResultsById.class.getName());
 
     public SauceTestResultsById(String id, SauceCredentials credentials) {
         this.id = id;
         this.credentials = credentials;
+        this.job = new JenkinsJobInformation(id, credentials.getHMAC(id));
+        JenkinsSauceREST sauceREST = new JenkinsSauceREST(credentials.getUsername(), credentials.getPassword().getPlainText());
+
+        try {
+            String jsonResponse = sauceREST.getJobInfo(id);
+            JSONObject jsonObject = new JSONObject(jsonResponse);
+            this.job.populateFromJson(jsonObject);
+        } catch (JSONException e) {
+            logger.log(Level.WARNING, "Unable to retrieve Job data from Sauce Labs", e);
+        }
+
     }
 
     public String getId() {
@@ -24,4 +45,16 @@ public class SauceTestResultsById {
         return credentials.getHMAC(this.id);
     }
 
+    public String getName() {
+        return job.getName();
+    }
+
+    public String getFailureMessage() {
+        return job.getFailureMessage();
+    }
+
+    public String getJobId() {
+        return job.getJobId();
+
+    }
 }
