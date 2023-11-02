6b401cbc1949e9e6880f7d86d68a523c705f9441
Diego Molina <diemol@gmail.com>
2023-11-02 16:31:06 +0100

    Adding a constructor with JenkinsSauceREST, so we can pass the mock.

--- src/main/java/hudson/plugins/sauce_ondemand/SauceTestResultsById.java
+++ src/main/java/hudson/plugins/sauce_ondemand/SauceTestResultsById.java
@@ -9,63 +9,65 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.json.JSONException;
 
-/**
- *
- */
+/** */
 public class SauceTestResultsById {
-    private final String id;
-    private final SauceCredentials credentials;
-    private final JenkinsJobInformation job;
-    private final String server;
+  /** Logger instance. */
+  private static final Logger logger = Logger.getLogger(SauceTestResultsById.class.getName());
 
-    /**
-     * Logger instance.
-     */
-    private static final Logger logger = Logger.getLogger(SauceTestResultsById.class.getName());
+  private final String id;
+  private final SauceCredentials credentials;
+  private final JenkinsJobInformation job;
+  private final String server;
 
-    public SauceTestResultsById(String id, SauceCredentials credentials) {
-        this.id = id;
-        this.credentials = credentials;
-        this.job = new JenkinsJobInformation(id, credentials.getHMAC(id));
-        this.server = credentials.getRestEndpoint().replace("https://","https://app.");
-        DataCenter dc = DataCenter.fromString(credentials.getRestEndpointName());
-        JenkinsSauceREST sauceREST = new JenkinsSauceREST(credentials.getUsername(), credentials.getPassword().getPlainText(), dc);
-        JobsEndpoint jobs = sauceREST.getJobsEndpoint();
-
-        try {
-            Job job = jobs.getJobDetails(id);
-            this.job.populate(job);
-        } catch (JSONException|IOException e) {
-            logger.log(Level.WARNING, "Unable to retrieve Job data from Sauce Labs", e);
-        }
+  public SauceTestResultsById(String id, SauceCredentials credentials, JenkinsSauceREST sauceREST) {
+    this.id = id;
+    this.credentials = credentials;
+    this.job = new JenkinsJobInformation(id, credentials.getHMAC(id));
+    this.server = credentials.getRestEndpoint().replace("https://", "https://app.");
+    JobsEndpoint jobs = sauceREST.getJobsEndpoint();
+    try {
+      Job job = jobs.getJobDetails(id);
+      this.job.populate(job);
+    } catch (JSONException | IOException e) {
+      logger.log(Level.WARNING, "Unable to retrieve Job data from Sauce Labs", e);
     }
+  }
 
-    public String getId() {
-        return id;
-    }
+  public SauceTestResultsById(String id, SauceCredentials credentials) {
+    this(
+        id,
+        credentials,
+        new JenkinsSauceREST(
+            credentials.getUsername(),
+            credentials.getPassword().getPlainText(),
+            DataCenter.fromString(credentials.getRestEndpointName())));
+  }
 
-    public String getAuth() throws IOException {
-        return credentials.getHMAC(this.id);
-    }
+  public String getId() {
+    return id;
+  }
 
-    public String getServer() {
-        return server;
-    }
+  public String getAuth() throws IOException {
+    return credentials.getHMAC(this.id);
+  }
 
-    public String getName() {
-        return job.getName();
-    }
+  public String getServer() {
+    return server;
+  }
 
-    public boolean hasFailureMessage() {
-        return job.hasFailureMessage();
-    }
+  public String getName() {
+    return job.getName();
+  }
 
-    public String getFailureMessage() {
-        return job.getFailureMessage();
-    }
+  public boolean hasFailureMessage() {
+    return job.hasFailureMessage();
+  }
 
-    public String getJobId() {
-        return job.getJobId();
+  public String getFailureMessage() {
+    return job.getFailureMessage();
+  }
 
-    }
+  public String getJobId() {
+    return job.getJobId();
+  }
 }
