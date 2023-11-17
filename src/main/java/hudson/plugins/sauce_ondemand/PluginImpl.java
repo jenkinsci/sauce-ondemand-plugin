ff59fd8c451188e188c6eddab1dd87e7e0fdc170
Diego Molina <diemol@gmail.com>
2023-11-17 19:01:25 +0100

    Applying code styling

--- src/main/java/hudson/plugins/sauce_ondemand/PluginImpl.java
+++ src/main/java/hudson/plugins/sauce_ondemand/PluginImpl.java
@@ -52,198 +52,195 @@ import org.kohsuke.stapler.StaplerRequest;
 @Extension
 public class PluginImpl extends Plugin implements Describable<PluginImpl> {
 
-    private static final Logger logger = Logger.getLogger(PluginImpl.class.getName());
+  /** Handles the retrieval of browsers from Sauce Labs. */
+  static final BrowserFactory BROWSER_FACTORY =
+      BrowserFactory.getInstance(new JenkinsSauceREST(null, null, DataCenter.US_WEST));
+  private static final Logger logger = Logger.getLogger(PluginImpl.class.getName());
+  /** Username to access Sauce OnDemand. */
+  @Deprecated private transient String username;
 
-    /**
-     * Handles the retrieval of browsers from Sauce Labs.
-     */
-    static transient final BrowserFactory BROWSER_FACTORY = BrowserFactory.getInstance(new JenkinsSauceREST(null, null, DataCenter.US_WEST));
+  /** Password for Sauce OnDemand. */
+  @Deprecated private transient Secret apiKey;
 
-    /**
-     * User name to access Sauce OnDemand.
-     */
-    @Deprecated
-    private transient String username;
+  /** Data center endpoint for Sauce OnDemand. */
+  @Deprecated private transient String restEndpoint;
 
-    /**
-     * Password for Sauce OnDemand.
-     */
-    @Deprecated
-    private transient Secret apiKey;
+  private boolean reuseSauceAuth;
 
-    /**
-     * Data center endpoint for Sauce OnDemand.
-     */
-    @Deprecated
-    private transient String restEndpoint;
+  private String sauceConnectDirectory;
 
-    private boolean reuseSauceAuth;
+  private String sauceConnectOptions;
 
-    private String sauceConnectDirectory;
+  private String sauceConnectMaxRetries;
 
-    private String sauceConnectOptions;
+  private String sauceConnectRetryWaitTime;
 
-    private String sauceConnectMaxRetries;
+  @Deprecated private transient boolean disableStatusColumn;
 
-    private String sauceConnectRetryWaitTime;
+  private String environmentVariablePrefix;
 
-    @Deprecated
-    private transient boolean disableStatusColumn;
+  private transient Boolean sendUsageData;
 
-    private String environmentVariablePrefix;
+  private boolean disableUsageStats;
 
-    private transient Boolean sendUsageData;
+  private String credentialId;
 
-    private boolean disableUsageStats;
-
-    private String credentialId;
-
-    protected Object readResolve() {
-        if (sendUsageData != null) {
-           disableUsageStats = false;
-        }
-        return this;
+  public static PluginImpl get() {
+    Jenkins j = Jenkins.getInstance();
+    if (j == null) {
+      return null;
     }
+    return j.getPlugin(PluginImpl.class);
+  }
 
+  protected Object readResolve() {
+    if (sendUsageData != null) {
+      disableUsageStats = false;
+    }
+    return this;
+  }
+
+  @Override
+  public void start() throws Exception {
+    // Register the small (16x16) icons...
+    IconSet.icons.addIcon(
+        new Icon(
+            "icon-sauce-ondemand-credential icon-sm",
+            "sauce-ondemand/images/16x16/sauce-logo-sm.png",
+            Icon.ICON_SMALL_STYLE,
+            IconType.PLUGIN));
+
+    // Register the medium (24x24) icons...
+    IconSet.icons.addIcon(
+        new Icon(
+            "icon-sauce-ondemand-credential icon-md",
+            "sauce-ondemand/images/24x24/sauce-logo-md.png",
+            Icon.ICON_MEDIUM_STYLE,
+            IconType.PLUGIN));
+
+    // Register the large (32x32) icons...
+    IconSet.icons.addIcon(
+        new Icon(
+            "icon-sauce-ondemand-credential icon-lg",
+            "sauce-ondemand/images/32x32/sauce-logo-lg.png",
+            Icon.ICON_LARGE_STYLE,
+            IconType.PLUGIN));
+
+    // Register the x-large (48x48) icons...
+    IconSet.icons.addIcon(
+        new Icon(
+            "icon-sauce-ondemand-credential icon-xlg",
+            "sauce-ondemand/images/48x48/sauce-logo-xlg.png",
+            Icon.ICON_XLARGE_STYLE,
+            IconType.PLUGIN));
+    // backward compatibility with the legacy class name
+    Items.XSTREAM.alias(
+        "hudson.plugins.sauce_ondemand.SoDBuildWrapper", SauceOnDemandBuildWrapper.class);
+    Items.XSTREAM.alias(
+        "hudson.plugins.sauce__ondemand.SoDBuildWrapper", SauceOnDemandBuildWrapper.class);
+    // the real name must be registered at the end
+    Items.XSTREAM.alias(
+        "hudson.plugins.sauce_ondemand.SauceOnDemandBuildWrapper", SauceOnDemandBuildWrapper.class);
+
+    load();
+  }
+
+  @Override
+  public void configure(StaplerRequest req, JSONObject formData)
+      throws IOException, ServletException, Descriptor.FormException {
+    sauceConnectDirectory = formData.getString("sauceConnectDirectory");
+    sauceConnectOptions = formData.getString("sauceConnectOptions");
+    environmentVariablePrefix = formData.getString("environmentVariablePrefix");
+    setDisableUsageStats(formData.getBoolean("disableUsageStats"));
+    sauceConnectMaxRetries = formData.getString("sauceConnectMaxRetries");
+    sauceConnectRetryWaitTime = formData.getString("sauceConnectRetryWaitTime");
+    save();
+  }
+
+  public DescriptorImpl getDescriptor() {
+    Jenkins j = Jenkins.getInstance();
+    if (j == null) {
+      return null;
+    }
+    return (DescriptorImpl) j.getDescriptorOrDie(PluginImpl.class);
+  }
+
+  public String getSauceConnectDirectory() {
+    return sauceConnectDirectory;
+  }
+
+  public void setSauceConnectDirectory(String sauceConnectDirectory) {
+    this.sauceConnectDirectory = sauceConnectDirectory;
+  }
+
+  public String getEnvironmentVariablePrefix() {
+    return environmentVariablePrefix;
+  }
+
+  public void setEnvironmentVariablePrefix(String environmentVariablePrefix) {
+    this.environmentVariablePrefix = environmentVariablePrefix;
+  }
+
+  public String getSauceConnectMaxRetries() {
+    return sauceConnectMaxRetries;
+  }
+
+  public void setSauceConnectMaxRetries(String sauceConnectMaxRetries) {
+    this.sauceConnectMaxRetries = sauceConnectMaxRetries;
+  }
+
+  public String getSauceConnectRetryWaitTime() {
+    return sauceConnectRetryWaitTime;
+  }
+
+  public void setSauceConnectRetryWaitTime(String sauceConnectRetryWaitTime) {
+    this.sauceConnectRetryWaitTime = sauceConnectRetryWaitTime;
+  }
+
+  @Deprecated
+  public String getUsername() {
+    return username;
+  }
+
+  @Deprecated
+  public Secret getApiKey() {
+    return apiKey;
+  }
+
+  @Deprecated
+  public String getRestEndpoint() {
+    return restEndpoint;
+  }
+
+  public String getSauceConnectOptions() {
+    return sauceConnectOptions;
+  }
+
+  public void setSauceConnectOptions(String sauceConnectOptions) {
+    this.sauceConnectOptions = sauceConnectOptions;
+  }
+
+  public void setDisableUsageStats(boolean disableUsageStats) {
+    this.disableUsageStats = disableUsageStats;
+  }
+
+  public boolean isDisableUsageStats() {
+    return disableUsageStats;
+  }
+
+  @Extension
+  public static final class DescriptorImpl extends Descriptor<PluginImpl> {
     @Override
-    public void start() throws Exception {
-        // Register the small (16x16) icons...
-        IconSet.icons.addIcon(new Icon(
-                "icon-sauce-ondemand-credential icon-sm",
-                "sauce-ondemand/images/16x16/sauce-logo-sm.png",
-                Icon.ICON_SMALL_STYLE, IconType.PLUGIN)
-        );
-
-        // Register the medium (24x24) icons...
-        IconSet.icons.addIcon(new Icon(
-                "icon-sauce-ondemand-credential icon-md",
-                "sauce-ondemand/images/24x24/sauce-logo-md.png",
-                Icon.ICON_MEDIUM_STYLE, IconType.PLUGIN)
-        );
-
-        // Register the large (32x32) icons...
-        IconSet.icons.addIcon(new Icon(
-                "icon-sauce-ondemand-credential icon-lg",
-                "sauce-ondemand/images/32x32/sauce-logo-lg.png",
-                Icon.ICON_LARGE_STYLE, IconType.PLUGIN)
-        );
-
-        // Register the x-large (48x48) icons...
-        IconSet.icons.addIcon(new Icon(
-                "icon-sauce-ondemand-credential icon-xlg",
-                "sauce-ondemand/images/48x48/sauce-logo-xlg.png",
-                Icon.ICON_XLARGE_STYLE, IconType.PLUGIN)
-        );
-        // backward compatibility with the legacy class name
-        Items.XSTREAM.alias("hudson.plugins.sauce_ondemand.SoDBuildWrapper", SauceOnDemandBuildWrapper.class);
-        Items.XSTREAM.alias("hudson.plugins.sauce__ondemand.SoDBuildWrapper", SauceOnDemandBuildWrapper.class);
-        // the real name must be registered at the end
-        Items.XSTREAM.alias("hudson.plugins.sauce_ondemand.SauceOnDemandBuildWrapper", SauceOnDemandBuildWrapper.class);
-
-        load();
+    public String getDisplayName() {
+      return "Sauce OnDemand";
     }
 
-
-    @Override
-    public void configure(StaplerRequest req, JSONObject formData) throws IOException, ServletException, Descriptor.FormException {
-        sauceConnectDirectory = formData.getString("sauceConnectDirectory");
-        sauceConnectOptions = formData.getString("sauceConnectOptions");
-        environmentVariablePrefix = formData.getString("environmentVariablePrefix");
-        setDisableUsageStats(formData.getBoolean("disableUsageStats"));
-        sauceConnectMaxRetries = formData.getString("sauceConnectMaxRetries");
-        sauceConnectRetryWaitTime = formData.getString("sauceConnectRetryWaitTime");
-        save();
+    /**
+     * @param context Ancestor/what project
+     * @return the list of supported credentials
+     */
+    public ListBoxModel doFillCredentialIdItems(final @AncestorInPath ItemGroup<?> context) {
+      return new StandardUsernameListBoxModel().withAll(SauceCredentials.all(context));
     }
-
-    public DescriptorImpl getDescriptor() {
-        Jenkins j = Jenkins.getInstance();
-        if (j == null) { return null; }
-        return (DescriptorImpl) j.getDescriptorOrDie(PluginImpl.class);
-    }
-
-    public static PluginImpl get() {
-        Jenkins j = Jenkins.getInstance();
-        if (j == null) { return null; }
-        return j.getPlugin(PluginImpl.class);
-    }
-
-    public String getSauceConnectDirectory() {
-        return sauceConnectDirectory;
-    }
-
-    public void setSauceConnectDirectory(String sauceConnectDirectory) {
-        this.sauceConnectDirectory = sauceConnectDirectory;
-    }
-
-    public String getEnvironmentVariablePrefix() {
-        return environmentVariablePrefix;
-    }
-
-    public void setEnvironmentVariablePrefix(String environmentVariablePrefix) {
-        this.environmentVariablePrefix = environmentVariablePrefix;
-    }
-
-    public String getSauceConnectMaxRetries() {
-        return sauceConnectMaxRetries;
-    }
-
-    public void setSauceConnectMaxRetries(String sauceConnectMaxRetries) {
-        this.sauceConnectMaxRetries = sauceConnectMaxRetries;
-    }
-
-    public String getSauceConnectRetryWaitTime() {
-        return sauceConnectRetryWaitTime;
-    }
-
-    public void setSauceConnectRetryWaitTime(String sauceConnectRetryWaitTime) {
-        this.sauceConnectRetryWaitTime = sauceConnectRetryWaitTime;
-    }
-
-    @Deprecated
-    public String getUsername() {
-        return username;
-    }
-
-    @Deprecated
-    public Secret getApiKey() {
-        return apiKey;
-    }
-
-    @Deprecated
-    public String getRestEndpoint() {
-        return restEndpoint;
-    }
-
-    @Extension
-    public static final class DescriptorImpl extends Descriptor<PluginImpl> {
-        @Override
-        public String getDisplayName() {
-            return "Sauce OnDemand";
-        }
-
-        /**
-         * @param context    Ancestor/what project
-         * @return the list of supported credentials
-         */
-        public ListBoxModel doFillCredentialIdItems(final @AncestorInPath ItemGroup<?> context) {
-            return new StandardUsernameListBoxModel()
-                .withAll(SauceCredentials.all(context));
-        }
-    }
-
-    public String getSauceConnectOptions() {
-        return sauceConnectOptions;
-    }
-
-    public void setSauceConnectOptions(String sauceConnectOptions) {
-        this.sauceConnectOptions = sauceConnectOptions;
-    }
-
-    public void setDisableUsageStats(boolean disableUsageStats) {
-        this.disableUsageStats = disableUsageStats;
-    }
-
-    public boolean isDisableUsageStats() {
-        return disableUsageStats;
-    }
+  }
 }
