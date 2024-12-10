c9585f247dec99bd174bb22f4be5dcc417ec3f9f
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-12-10 09:05:07 -0500

    extract inline JS script in SauceOnDemandBuildWrapper/config.jelly

---
+++ src/main/resources/hudson/plugins/sauce_ondemand/SauceOnDemandBuildWrapper/sauce-chosen-init.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+  new Chosen(document.getElementById('webDriverBrowsers'));
+  new Chosen(document.getElementById('appiumBrowsers'));
+});
