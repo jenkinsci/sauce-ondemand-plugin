c4036fb1922da71ca58adf5e3f697eecefd2c246
Sebastian Buczyński <sebastian.buczynski@saucelabs.com>
2025-06-10 17:22:59 +0200

    Use latest ci-sauce

--- src/test/java/hudson/plugins/sauce_ondemand/ExtractSauceConnectTest.java
+++ src/test/java/hudson/plugins/sauce_ondemand/ExtractSauceConnectTest.java
@@ -4,6 +4,7 @@ import com.saucelabs.ci.sauceconnect.SauceConnectManager;
 import java.io.File;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.helpers.NOPLogger;
 
 /**
  * @author Ross Rowe
@@ -20,11 +21,11 @@ public class ExtractSauceConnectTest {
     @Test
     public void linux() throws Exception {
         File workingDirectory = new File(System.getProperty("java.io.tmpdir"));
-        manager.extractZipFile(workingDirectory, SauceConnectManager.OperatingSystem.LINUX_AMD64);
+        manager.extractZipFile(workingDirectory, SauceConnectManager.OperatingSystem.LINUX_AMD64, NOPLogger.NOP_LOGGER);
     }
 
     @Test
     public void windows() throws Exception {
-        manager.extractZipFile(new File(System.getProperty("java.io.tmpdir")), SauceConnectManager.OperatingSystem.WINDOWS_AMD64);
+        manager.extractZipFile(new File(System.getProperty("java.io.tmpdir")), SauceConnectManager.OperatingSystem.WINDOWS_AMD64, NOPLogger.NOP_LOGGER);
     }
 }
